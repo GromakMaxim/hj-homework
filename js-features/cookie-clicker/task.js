2e00f252847adf97d4f1e27c057dbfad6d7e1395
Gromak Maxim <maximgromak@gmail.com>
2021-07-04 15:49:15 +1000

    upd: task 1.2

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,9 +1,18 @@
 let clickCounter = document.getElementById("clicker__counter");
 let cookiePicture = document.getElementById("cookie");
+let clickSpeed = document.getElementById("click_per_sec");
+
+let firstClick = new Date();
 
 cookiePicture.onclick = function () {
+    let secondClick = new Date();
+    let timeBetweenClicks = (secondClick - firstClick) / 1000;
+    let clickPerSecond = 1 / timeBetweenClicks;
+
+    firstClick = secondClick;
+    clickSpeed.textContent = Math.ceil(clickPerSecond * 100) / 100;
+
     clickCounter.textContent = ++clickCounter.textContent;
-    console.log(cookiePicture.width)
     if (cookiePicture.width == 200) {
         cookiePicture.width = 250;
     } else if (cookiePicture.width == 250) {
