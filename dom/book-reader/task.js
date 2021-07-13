7a4a57e9b5aa59fd401dab3b9a43972fd54eb4c6
Gromak Maxim <maximgromak@gmail.com>
2021-07-14 02:54:02 +1000

    upd: task 4.3

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,29 +1,68 @@
+//UI buttons
 let fontButtons = document.getElementsByClassName("font-size");
-let book = document.getElementById("book");
+let fontColorButtons = document.getElementsByClassName("book__control_color").item(0).children;
+let bgColorButtons = document.getElementsByClassName("book__control_background").item(0).children;
 
+//managing parameters
+let book = document.getElementById("book");
+let parameters = { fontSize: "", fontColor: "", bgColor: "" };
+
+//font size
 for (let btn of fontButtons) {
     btn.addEventListener("click", function (event) {
         event.preventDefault();
-        switchActiveIcon(btn);
-        if (btn.dataset.size === "small") changeFont("book_fs-small");
-        else if (btn.dataset.size === "big") changeFont("book_fs-big");
-        else changeFont("");
+        switchActiveIcon(btn, "font-size_active", fontButtons);
+        if (btn.dataset.size === "small") changeParameter("book_fs-small");
+        else if (btn.dataset.size === "big") changeParameter("book_fs-big");
+        else changeParameter("book_fs-normal");
     });
 }
 
-function deactivate() {
-    for (let btn of fontButtons) {
-        btn.classList.remove("font-size_active");
+//font-color
+for (let btn of fontColorButtons) {
+    btn.addEventListener("click", function (event) {
+        event.preventDefault();
+        switchActiveIcon(btn, "color_active", fontColorButtons);
+        if (btn.dataset.textColor === "black") changeParameter("book_color-black");
+        else if (btn.dataset.textColor === "gray") changeParameter("book_color-gray");
+        else if (btn.dataset.textColor === "whitesmoke") changeParameter("book_color-whitesmoke");
+    })
+}
+
+//bg
+for (let btn of bgColorButtons) {
+    btn.addEventListener("click", function (event) {
+        event.preventDefault();
+        switchActiveIcon(btn, "color_active", bgColorButtons);
+        if (btn.dataset.bgColor === "black") changeParameter("book_bg-black");
+        else if (btn.dataset.bgColor === "gray") changeParameter("book_bg-gray");
+        else if (btn.dataset.bgColor === "white") changeParameter("book_bg-white");
+    })
+}
+
+
+function deactivate(option, arr) {
+    for (let btn of arr) {
+        btn.classList.remove(option);
     }
 }
 
-function switchActiveIcon(btn) {
-    deactivate();
-    btn.classList.add("font-size_active");
+function switchActiveIcon(btn, option, arr) {
+    deactivate(option, arr);
+    btn.classList.add(option);
 }
 
-function changeFont(option) {
-    book.className = "";
+function changeParameter(option) {
+    if (option.indexOf("book_fs-normal") !== -1) {
+        book.classList.remove("book_fs-small");
+        book.classList.remove("book_fs-big");
+    }
+    if (option.indexOf("_fs") !== -1) parameters.fontSize = option;
+    if (option.indexOf("_color") !== -1) parameters.fontColor = option;
+    if (option.indexOf("_bg") !== -1) parameters.bgColor = option;
+
     book.className = "book";
-    if (option !== "") book.classList.toggle(option);
+    if (parameters.fontColor != "") book.classList.add(parameters.fontColor);
+    if (parameters.fontSize != "") book.classList.add(parameters.fontSize);
+    if (parameters.bgColor != "") book.classList.add(parameters.bgColor);
 }
