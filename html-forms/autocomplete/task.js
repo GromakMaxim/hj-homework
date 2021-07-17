ca780a3dad849c64d7dd8f9a35a5ca256352f3f7
Gromak Maxim <maximgromak@gmail.com>
2021-07-17 18:44:30 +1000

    task 5.3

--- html-forms/autocomplete/task.js
+++ html-forms/autocomplete/task.js
@@ -67,26 +67,25 @@ class Autocomplete {
     this.list.innerHTML = html.join('');
   }
 
-  getMatches( text ) {
-    /*
-      TODO: этот метод нужно дописать
-      text - фраза, которую вводят в поле поиска
-      Метод должен вернуть массив.
+    getMatches(text) {
+        let autocompleteWords = this.input;
+        let resultArr = new Array();
+        let len = text.length;
 
-      Он формируется на основе списка опций select-элемента (this.input)
-      Подходящие опции - те, чей текст содержит то, что есть в аргументе text
-      Необходимо вернуть массив объектов со свойствами:
-      {
-        text: 'Содержимое <option>',
-        value: 'Содержимое атрибута value'
-      }
-    */
-    return [
-      {
-        text: 'Чубакка',
-        value: '1'
-      }
-    ];
+        for (let word of autocompleteWords) {
+            let inputPart = text.substring(0, len);
+            let wordPart = word.textContent.substring(0, len);
+
+            if (inputPart.match(wordPart)) {
+                resultArr.push(
+                    {
+                        text: word.textContent,
+                        value: word.value
+                    }
+                );
+            }
+        }
+        return resultArr;
   }
 }
 
