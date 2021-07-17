78eaff1ee1914d08a70b3ff27a7f21af753c2ccf
Gromak Maxim <maximgromak@gmail.com>
2021-07-17 21:57:09 +1000

    task 6.2

---
+++ document-structure/todo/task.js
@@ -0,0 +1,36 @@
+let addButton = document.getElementById("tasks__add");
+let userInput = document.getElementsByClassName("tasks__input").item(0);
+let taskList = document.getElementById("tasks__list");
+
+addButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    remove(
+        add(userInput.value)
+    );
+});
+
+
+function add(txt) {
+    let taskElement = document.createElement("div");
+    taskElement.className = "task";
+    let taskTitle = document.createElement("div");
+    taskTitle.className = "task__title";
+    taskTitle.textContent = txt;
+    let deleteBtn = document.createElement("a");
+    deleteBtn.href = "#"
+    deleteBtn.className = "task__remove";
+    deleteBtn.textContent = "x";
+
+    taskElement.insertAdjacentElement("afterbegin", taskTitle);
+    taskElement.insertAdjacentElement("beforeend", deleteBtn);
+    taskList.insertAdjacentElement("afterbegin", taskElement);
+
+    return taskElement;
+}
+
+function remove(el) {
+    el.addEventListener("click", function (event) {
+        event.preventDefault();
+        btn.parentElement.remove();
+    });
+}
