95b9c365752c272dccfcede4b68b0645934aa189
Gromak Maxim <maximgromak@gmail.com>
2021-07-18 23:59:30 +1000

    fixed: task 6.1

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -10,10 +10,15 @@ for (let link of links) {
 function showTip(link) {
     let txt = link.title;
     if (link.getElementsByClassName("tooltip").item(0) == null) {
+        let pos = link.getBoundingClientRect();
         let el = document.createElement("div");
+        console.log(pos)
         el.classList.add("tooltip");
         el.classList.add("tooltip_active");
         el.textContent = txt;
+
+        el.style.left = pos.left + "px";
+        el.style.top = (pos.top + 20) + "px";
         link.insertAdjacentElement("beforeend", el);
     } else {
         link.getElementsByClassName("tooltip").item(0).remove();
